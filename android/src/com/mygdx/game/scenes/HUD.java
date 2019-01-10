c7d43d18d5d1aacc5ff95b16bf71db5932988bed
Statthem <alex-lit@ukr.net>
2019-01-10 23:33:26 +0200

    tiltled map added

---
+++ android/src/com/mygdx/game/scenes/HUD.java
@@ -0,0 +1,49 @@
+package com.mygdx.game.scenes;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class HUD {
+    public Stage stage;
+    private Viewport viewport;
+
+    private Integer fightTimer;
+    private Integer score;
+
+    Label fighTiimerLabel;
+    Label scoreLabel;
+    Label player1Label;
+    Label player2Label;
+
+    public HUD(SpriteBatch batch){
+        fightTimer = 90;
+        score = 0;
+
+        viewport = new StretchViewport(800, 480);
+        stage = new Stage(viewport, batch);
+
+        Table table = new Table();
+        table.top();
+        table.setFillParent(true);
+
+        fighTiimerLabel = new Label("TIME:" + String.format("%03d", fightTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        scoreLabel =  new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        player1Label = new Label("Player 1",new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        player2Label = new Label("Player 2",new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+        table.add(player1Label).expandX().padTop(10).padLeft(20);
+        table.add(player2Label).expandX().padTop(10).padRight(20);
+        table.row();
+        table.add(scoreLabel).expandX();
+        table.add(fighTiimerLabel).expand();
+
+        stage.addActor(table);
+
+    }
+}
